da76091cd0d0961764b97d6a9a2dd4ece61819dc
AnhTiu <ngoducmanhhb@gmail.com>
2025-04-04 20:08:21 +0700

    a

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,50 @@
 const User = require('../model/user')
 const asyncHandler = require('express-async-handler')
+
+
 const register = asyncHandler(async (req, res) => {
     const {email, password, firstname, lastname} = req.body
     if(!email || !password || !firstname || !lastname) 
     return res.status(400).json({
         success: false,
-        message: 'Missing input'
+        mes: 'Missing input'
     })
-    const response = await Users.create(req.body)
-    return res.status(200).json({
-        success: response ? true : false,
-        response
-        //message: response ? 'User created successfully' : 'Failed to create user',
-        
+
+    const user = await User.findOne({email})
+    if(user)
+        throw new Error('User has already been registered')
+    else{
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success : newUser ? true : false,
+            mes : newUser ? 'User created successfully!! Please login' : 'Something went wrong'
+        })
+    }
+})
+
+const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+    if(!email || !password) 
+    return res.status(400).json({
+        success: false,
+        mes: 'Missing input'
     })
+    //plain object
+    const response = await User.findOne({email})
+    if(response && await response.isCorrectPassword(password)) {
+        const {password, role, ...userData} = response.toObject()
+        return res.status(200).json({
+            success: true,
+            userData
+        })
+    }else {
+        throw new Error('Invalid email or password')
+    }
+
+
 })
 
 module.exports = {
-    register
+    register,
+    login
 }
--- routers/user.js
+++ routers/user.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const ctrl = require('../controllers/user')
 
 router.post('/register', ctrl.register) //create - POST, read - GET, update - PUT, delete - DELETE
+router.post('/login', ctrl.login)
 
 module.exports = router
